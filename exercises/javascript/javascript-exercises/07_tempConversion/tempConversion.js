5d4589d219d8dba84bf1fc45b043bc87f17491f2
kutay yuceak <kutay_as@hotmail.com>
2024-12-25 02:54:19 +0300

    ui added for rps

--- exercises/javascript/javascript-exercises/07_tempConversion/tempConversion.js
+++ exercises/javascript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  result = (fahrenheit - 32) * (5/9)
+
+  let rounded = Math.round(result * 10) / 10;
+
+  return  rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+
+  result = (celcius * (9/5)) + 32;
+
+
+  let rounded = Math.round(result * 10) / 10;
+
+  return rounded;
+
 };
 
 // Do not edit below this line
